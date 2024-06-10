8e8e457db847835e9cfe265205ec95a375caffcc
Vadamee1 <david.hernandez.v44@gmail.com>
2024-06-10 14:13:48 -0600

    feat(home): about me

--- src/components/page/home/Welcome.tsx
+++ src/components/page/home/Welcome.tsx
@@ -7,12 +7,12 @@ import Typewriter from "typewriter-effect";
 export default function Welcome() {
   return (
     <Card isBlurred className="md:w-1/3">
-      <CardHeader className="flex justify-end">
+      {/* <CardHeader className="flex justify-end">
         <Button color="warning" variant="ghost">
           About me
         </Button>
-      </CardHeader>
-      <div className={`text-3xl ${typewriterFont.className} ml-5 mb-5`}>
+      </CardHeader> */}
+      <div className={`text-3xl ${typewriterFont.className} ml-5 mb-5 mt-5`}>
         <Typewriter
           options={{
             strings: welcome,
